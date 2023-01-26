f1d00342866825e5ca7484cc646fd52452d2a46e
Axl-M <axlmalyasov@gmail.com>
2023-01-26 18:26:11 +1000

    class Question created, tested - ok

--- question_game/questions.py
+++ question_game/questions.py
@@ -1,22 +1,45 @@
-class Question:
-    def __int__(self):
-        pass
+class Question():
+    def __init__(self, question_text, question_diff, question_answer):
+        self.question_text = question_text
+        self.question_diff = question_diff
+        self.question_answer = question_answer
+
+        self.is_asked = False
+        self.user_answer = None
+        self.points = self.question_diff * 10
 
     def get_points(self):
-        pass
+        """ Получить к-во очков """
+        return self.points
 
 
-    def is_corect(self):
-        pass
+    def is_correct(self):
+        """ Проверка правильности ответа """
+        return self.user_answer == self.question_answer
 
 
     def build_question(self):
-        pass
+        return f'Вопрос: {self.question_text}\nСложность {self.question_diff}/5'
 
 
     def build_positive_feedback(self):
-        pass
+        return f'Ответ верный, получено {self.points} баллов'
 
 
     def build_negative_feedback(self):
-        pass
+        return f'Ответ НЕВЕРНЫЙ, верный ответ - {self.question_answer} '
+
+
+
+# question
+# difficulty
+# correct answer
+data = {
+    "q": "How many days we have in week?",
+    "d": "1",
+    "a": "7"
+}
+
+
+
+q_10 = Question(data.get("q"), data.get("d"), data.get("a"))
