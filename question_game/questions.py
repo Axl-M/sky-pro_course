e7c677b8ea8ab9c9b6827f112ffb729a2d7bb15a
Axl-M <axlmalyasov@gmail.com>
2023-01-26 17:20:45 +1000

    project Question_game commenced

---
+++ question_game/questions.py
@@ -0,0 +1,22 @@
+class Question:
+    def __int__(self):
+        pass
+
+    def get_points(self):
+        pass
+
+
+    def is_corect(self):
+        pass
+
+
+    def build_question(self):
+        pass
+
+
+    def build_positive_feedback(self):
+        pass
+
+
+    def build_negative_feedback(self):
+        pass
