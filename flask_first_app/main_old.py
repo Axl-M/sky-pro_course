659db08082436fc3df21a0b3d486ff691293a4a3
Axl-M <axlmalyasov@gmail.com>
2023-02-04 21:56:53 +1000

    refactoring

---
+++ flask_first_app/main_old.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import json
+import utils
+from classes.candidates_getter import CandidatesGetter
+
+candidate_getter = CandidatesGetter('candidates.json')
+
+app = Flask(__name__)
+
+candidates = utils.load_candidates()
+
+# assert(False)   # остановка выполнения программы
+
+@app.route('/')
+def page_index():
+    """
+    Выводит инфо по всем кандидатам
+    :return: всё инфо
+    """
+    str_candidates = '<pre>'    # <pre> чтобы \n корректно отрабатывались
+    for candidate in candidates.values():
+        str_candidates += f'Имя кандидата - {candidate["name"]}\n'
+        str_candidates += f'Позиция кандидата - {candidate["position"]}\n'
+        str_candidates += f'Навыки - {candidate["skills"]}\n\n'
+    str_candidates += '</pre>'
+    return str_candidates
+
+
+@app.route('/candidate/<int:id>')
+def page_profile(id):
+    """
+    выводит инфо по конкретному кандидату
+    :param id: id кандидата
+    :return: инфо о кандидате
+    """
+    candidate = candidates[id]  # <br> тогда <pre> не нужен
+    str_candidates = f'<img src={candidate["picture"]}> <br>'
+    str_candidates += f'Имя кандидата - {candidate["name"]} <br>'
+    str_candidates += f'Позиция кандидата - {candidate["position"]} <br>'
+    str_candidates += f'Навыки - {candidate["skills"]} <br><br>'
+    return str_candidates
+
+@app.route('/skill/<skill>')    # параметром автоматически передается СТРОКА
+def page_skills(skill):
+    str_candidates = ''
+    for candidate in candidates.values():
+        candidate_skills = candidate['skills'].split(', ')
+        candidate_skills = [x.lower() for x in candidate_skills]
+        if skill.lower() in candidate_skills:
+            str_candidates += f'Имя кандидата - {candidate["name"]} <br>'
+            str_candidates += f'Позиция кандидата - {candidate["position"]} <br>'
+            str_candidates += f'Навыки - {candidate["skills"]} <br><br>'
+    return str_candidates
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)   # запуск сервера. debug - чтобы не перезагружать
--- flask_first_app/utils.py
+++ flask_first_app/utils.py
@@ -1,11 +1,17 @@
 import json
-from pprint import pprint
+# from pprint import pprint
 
-def load_candidates():
-    with open('candidates.json', encoding='utf8') as file:
+path = 'candidates.json'
+def load_candidates_from_json(path):
+    """
+    Возвращает список всех кандидатов
+    :param path:
+    :return:
+    """
+    with open(path, encoding='utf8') as file:
         data = json.load(file)
-        print(data)
-        # словарь (ключь - порядковый номер = значение id,
+        # print(data)
+        # словарь (ключ - порядковый номер = значение id,
         # значение - подсловарь/инфо о кандидате)
         # чтобы потом получать конкретного кандидата по id (ключ)
         candidates = {}
@@ -16,4 +22,46 @@ def load_candidates():
         # print(data)
         return candidates
 
-load_candidates()
+def get_candidate(candidate_id):
+    """
+    Возвращает одного кандидата по его id
+    :param candidate_id:
+    :return:
+    """
+    return candidates[candidate_id]
+
+def get_candidates_by_name(candidates, candidate_name):
+    """
+    Возвращает кандидатов по имени
+    :candidates: список всех кандидатов
+    :param candidate_name:
+    :return:
+    """
+    if candidate_name == '':
+        return 'Данные для поиска не указаны'
+
+    candidate_list = []
+    # print(candidates.values())
+    for candidate in candidates.values():
+        if candidate_name  == candidate['name']:
+            candidate_list.append(candidate)
+        else:
+            continue
+
+    return candidate_list
+
+
+def get_candidates_by_skill(skill_name):
+    """
+    Возвращает кандидатов по навыку
+    :param skill_name:
+    :return:
+    """
+    pass
+
+# test
+# print(load_candidates_from_json(path))
+candidates = load_candidates_from_json(path)
+# print(candidates)
+# print(get_candidates_by_name(candidates, "Наташа Приз"))
+print(get_candidate(6))
