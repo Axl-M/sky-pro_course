e4ecbb6895093bd92c265154d0a088d56378470b
Axl-M <axlmalyasov@gmail.com>
2023-02-05 14:14:36 +1000

    Class Candidate created

--- flask_first_app/classes/candidates_getter.py
+++ flask_first_app/classes/candidates_getter.py
@@ -1,3 +1,20 @@
+class Candidate:
+    def __init__(self, candidate_id: int, name: str, avatar: str, position: str, gender: str, age: str, skills: str):
+        self.candidate_id = candidate_id
+        self.name = name
+        self.avatar = avatar
+        self.position = position
+        self.gender = gender
+        self.age = age
+        self.skills = skills
+
+        print(f'Initialiation of instance ID {self.candidate_id} completed')
+
+    def __repr__(self):
+        return f'\nID: {self.candidate_id}\nName: {self.name}\nAvatar: {self.avatar}\n \
+        Position: {self.position}\nGender: {self.gender}\nAge: {self.age}\nSkills: {self.skills}'
+
+
 class CandidatesGetter:
     def __init__(self, path):
         self.path = path
--- flask_first_app/main.py
+++ flask_first_app/main.py
@@ -1,11 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import utils
 
 app = Flask(__name__)
+
+path = 'candidates.json'
+candidates = utils.load_candidates_from_json(path)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/all_candidates')
+def page_index():
+    """
+    Выводит инфо по всем кандидатам
+    :return: всё инфо
+    """
+    return render_template('all_candidates.html', candidates=candidates)
+
+
+@app.route('/candidates/<int:id>')
+def page_profile(id):
+    """
+    выводит инфо по конкретному кандидату
+    :param id: id кандидата
+    :return: инфо о кандидате
+    """
+    candidate = utils.get_candidate(id)
+    # candidate = candidates[id]  # <br> тогда <pre> не нужен
+    # str_candidates = f'<img src={candidate["picture"]}> <br>'
+    # str_candidates += f'Имя кандидата - {candidate["name"]} <br>'
+    # str_candidates += f'Позиция кандидата - {candidate["position"]} <br>'
+    # str_candidates += f'Навыки - {candidate["skills"]} <br><br>'
+    # return str_candidates
+    render_template('single.html', candidate=candidate)
+
+
+@app.route('/search/<candidate_name>')
+def search_candidate_name():
+    render_template('search.html')
+
+@app.route('/skill/<skill_name>')
+def search_candidate_skill():
+    render_template('skill.html')
 
 
 if __name__ == '__main__':
