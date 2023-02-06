ffafc24da59a41a7bfd737c3763f7113952e77cb
Axl-M <axlmalyasov@gmail.com>
2023-02-06 23:43:35 +1000

    all logic ok

--- flask_first_app/main.py
+++ flask_first_app/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import utils
+from config import path
 
 app = Flask(__name__)
 
-path = 'candidates.json'
-candidates = utils.load_candidates_from_json(path)
+data = utils.load_candidates_from_json(path)
+
+# candidate_dict, skill_list = utils.build_candidate_instances_and_skill_list(candidates)
 
 @app.route('/')
 def index():
@@ -12,12 +14,12 @@ def index():
 
 
 @app.route('/all_candidates')
-def page_index():
+def all_candidates():
     """
     Выводит инфо по всем кандидатам
     :return: всё инфо
     """
-    return render_template('all_candidates.html', candidates=candidates)
+    return render_template('all_candidates.html', candidates=data)
 
 
 @app.route('/candidates/<int:id>')
@@ -27,24 +29,45 @@ def page_profile(id):
     :param id: id кандидата
     :return: инфо о кандидате
     """
-    candidate = utils.get_candidate(id)
+    # candidate = utils.get_candidate(id)
     # candidate = candidates[id]  # <br> тогда <pre> не нужен
     # str_candidates = f'<img src={candidate["picture"]}> <br>'
     # str_candidates += f'Имя кандидата - {candidate["name"]} <br>'
     # str_candidates += f'Позиция кандидата - {candidate["position"]} <br>'
     # str_candidates += f'Навыки - {candidate["skills"]} <br><br>'
     # return str_candidates
-    render_template('single.html', candidate=candidate)
+    # render_template('single.html', candidate=candidate)
+    return render_template('single.html', candidate=utils.get_candidate(id))
+    # return utils.get_candidate(id)
+
+# print(utils.get_candidate(6))
+
+@app.route('/search/<name>')
+def search_candidate_name(name):
+    if name == '':
+        return 'Необходимо указать имя'
+    candidates = utils.get_candidates_by_name(name)
+    return render_template('search.html', candidates=candidates, candidates_len=len(candidates))
+    # return utils.get_candidates_by_name(name)
 
 
-@app.route('/search/<candidate_name>')
-def search_candidate_name():
-    render_template('search.html')
+# @app.route('/search_by_skill', method=['GET', 'POST'])
+# def page_search_by_skill():
+#     if request.method == 'POST':
+#         search_data = request.form.get('search_data_from_input').lower()
+#         return render_template('search_by_skill.html', search_data=get_candidates_by_skill(candidate_dict, search_data))
+#     return render_template('search_by_skill.html')
 
-@app.route('/skill/<skill_name>')
-def search_candidate_skill():
-    render_template('skill.html')
-
+# @app.route('/skill/', methods=['GET'])
+@app.route('/skill/<skill>', methods=['GET'])
+def search_candidate_by_skill(skill=None):
+    candidates = utils.get_candidates_by_skill(skill)
+    print(candidates)
+    # render_template('skill.html', skill=skill, skill_list=skill_list,
+    #                 search_data=get_candidate_by_skill(candidate_dict, skill))
+    return render_template('skill.html',  candidates=candidates, candidates_len=len(candidates), skill=skill)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
--- flask_first_app/utils.py
+++ flask_first_app/utils.py
@@ -1,36 +1,71 @@
 import json
+# from typing import Union
 # from pprint import pprint
+from config import path
+from classes.candidates_getter import Candidate
 
-path = 'candidates.json'
-def load_candidates_from_json(path):
+__data = []
+def load_candidates_from_json(path: str) -> dict:
     """
     Возвращает список всех кандидатов
     :param path:
     :return:
     """
+    global __data
     with open(path, encoding='utf8') as file:
-        data = json.load(file)
+        __data = json.load(file)
         # print(data)
         # словарь (ключ - порядковый номер = значение id,
         # значение - подсловарь/инфо о кандидате)
         # чтобы потом получать конкретного кандидата по id (ключ)
-        candidates = {}
-        for i in data:
-            candidates[i['id']] = i
+        # candidates = {}
+        # for i in data:
+        #     candidates[i['id']] = i
             # print(candidates[i['id']])
         # pprint(candidates)
         # print(data)
-        return candidates
+    # return candidates
+    return __data
 
-def get_candidate(candidate_id):
+
+def build_candidate_instances_and_skill_list(data: dict) -> tuple[dict, list]:
+    """
+    Object instance initializer (class Candidate)
+    :param data: dictionary of candidates
+    :return: list of objects and list of skiills
+    """
+    candidate_dict = {}
+    skill_list = []
+
+    for candidate_id, candidate_data in data.items():
+        candidate = Candidate(candidate_id=candidate_id, name=candidate_data['name'], avatar=candidate_data['picture'],
+                              position=candidate_data['position'], gender=candidate_data['gender'],
+                              age=candidate_data['age'], skills=candidate_data['skills'])
+        skill_list.extend([skill.lower() for skill in candidate.skills.split(', ')])
+        candidate_dict[candidate_id] = candidate
+
+    skill_list = list(set(skill_list))
+    return candidate_dict, skill_list
+
+def get_candidate(candidate_id: int):
     """
     Возвращает одного кандидата по его id
+    candidate_dict: Все кандидаты в словаре
     :param candidate_id:
-    :return:
+    :return: Экземпляр Candidate
     """
-    return candidates[candidate_id]
+    # for i in candidate_dict:
+    #     if candidate_id == i:
+    #         return candidate_dict[candidate_id]
+    for candidate in __data:
+        if candidate['id'] == candidate_id:
+            # print(candidate)
+            return candidate
 
-def get_candidates_by_name(candidates, candidate_name):
+    candidate = {'not_found': 'Данные не найдены'}
+    return candidate
+
+def get_candidates_by_name(candidate_name: str) -> list[Candidate] | str:
     """
     Возвращает кандидатов по имени
     :candidates: список всех кандидатов
@@ -40,35 +75,50 @@ def get_candidates_by_name(candidates, candidate_name):
     if candidate_name == '':
         return 'Данные для поиска не указаны'
 
-    candidate_list = []
-    # print(candidates.values())
-    for candidate in candidates.values():
-        if candidate_name  == candidate['name']:
-            candidate_list.append(candidate)
-        else:
-            continue
+    return [candidate for candidate in __data if candidate_name.lower() in candidate['name'].lower() ]
+    # candidate_list = []
+    # # print(candidates.values())
+    # for candidate in candidates.values():
+    #     if candidate_name  == candidate['name']:
+    #         candidate_list.append(candidate)
+    #     else:
+    #         continue
 
-    return candidate_list
+    # if len(candidate_list) == 0:
+    #     return 'По данному запросу ничего не найдено.'
+
+    # return candidate_list
 
 
-def get_candidates_by_skill(skill):
+def get_candidates_by_skill(skill_for_search: str) -> list[Candidate] | str:
     """
     Возвращает кандидатов по навыку
-    :param skill:
+    :param skill_for_search:
     :return:
     """
     candidates_list = []
-    for candidate in candidates.values():
-        candidate_skills = candidate['skills'].split(', ')
-        candidate_skills = [x.lower() for x in candidate_skills]
-        if skill.lower() in candidate_skills:
+
+    if skill_for_search == '':
+        return 'Данные для поиска не указаны'
+
+    for candidate in __data:
+        skills = candidate['skills'].lower().split(', ')
+        # for skill in skills:
+        if skill_for_search.lower() in skills:
             candidates_list.append(candidate)
+
+
+    # for candidate in candidates.values():
+    #     candidate_skills = candidate['skills'].split(', ')
+    #     candidate_skills = [x.lower() for x in candidate_skills]
+    #     if skill_for_search.lower() in candidate_skills:
+    #         candidates_list.append(candidate)
     return candidates_list
 
 # test
 # print(load_candidates_from_json(path))
-candidates = load_candidates_from_json(path)
+# candidates = load_candidates_from_json(path)
 # print(candidates)
 # print(get_candidates_by_name(candidates, "Наташа Приз"))
 # print(get_candidate(6))
-print(get_candidates_by_skill('golang'))
+# print(get_candidates_by_skill(candidates, 'golang'))
