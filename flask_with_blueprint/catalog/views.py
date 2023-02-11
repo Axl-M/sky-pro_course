c732068871d86c2800a00de168c21d58bc18e053
Axl-M <axlmalyasov@gmail.com>
2023-02-11 23:42:18 +1000

    catalog/templates errors corrected

--- flask_with_blueprint/catalog/views.py
+++ flask_with_blueprint/catalog/views.py
@@ -6,19 +6,30 @@ from flask import Blueprint, render_template
 catalog_blueprint = Blueprint('catalog_blueprint', __name__,
                               template_folder='templates')
 # Создаем вьюшку, используя в декораторе блюпринт вместо арр
-@catalog_blueprint.route('/catalog')
+@catalog_blueprint.route('/catalog/')
 # def profile_page():
 def catalog_page():
     # return "Я главная каталога"
     return render_template('main.html')
 
 # Создаем вьюшку, используя в декораторе блюпринт вместо арр
-@catalog_blueprint.route('/catalog/<cat>')
+@catalog_blueprint.route('/catalog/<cat>/')
 # def profilepage(cat):
 def category_page(cat):
+    """
+    страничка категории
+    :param cat:
+    :return:
+    """
     # return f'Я ст?раничка каталога категории {cat}'
-    return render_template('canegory.html')
+    return render_template('category.html')
 
-@catalog_blueprint.route('/catalog/<cat>/<int:item>')
-def item_page(item):
+@catalog_blueprint.route('/catalog/<cat>/<int:item>/')
+def item_page(cat, item):
+    """
+    страничка товара
+    :param cat:
+    :param item:
+    :return:
+    """
     return render_template('item.html')
