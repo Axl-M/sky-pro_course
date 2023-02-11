39628c63f84fe3b3b39d76cb0ae14e8e67a5a251
Axl-M <axlmalyasov@gmail.com>
2023-02-12 00:00:57 +1000

    messages/views.py added all tested

--- flask_with_blueprint/app.py
+++ flask_with_blueprint/app.py
@@ -1,8 +1,10 @@
-from flask import Flask,request, render_template
+from flask import Flask, render_template
 
 # Импортируем блюпринты из их пакетов
 from catalog.views import catalog_blueprint
 from profiles.views import profile_blueprint
+# Импортируем блюпринты из их пакетов
+from messages.views import messages_blueprint
 
 app = Flask(__name__)
 
@@ -10,5 +12,7 @@ app = Flask(__name__)
 app.register_blueprint(catalog_blueprint)
 # И второй тоже регистрируем
 app.register_blueprint(profile_blueprint)
+# Регистрируем блюпринт с указание префикса
+app.register_blueprint(messages_blueprint, url_prefix='/messages')
 
 app.run()
--- flask_with_blueprint/catalog/views.py
+++ flask_with_blueprint/catalog/views.py
@@ -1,19 +1,22 @@
 # Сперва импорттируем класс блюпринта
 from flask import Blueprint, render_template
 
+
 # Затем создаем новый блюпринт, выбираем для него имя
 # было ('profile_blueprint') считаю исправил на правильное
 catalog_blueprint = Blueprint('catalog_blueprint', __name__,
-                              template_folder='templates')
+                template_folder='templates', url_prefix='/catalog/')
+
+
 # Создаем вьюшку, используя в декораторе блюпринт вместо арр
-@catalog_blueprint.route('/catalog/')
+@catalog_blueprint.route('/')
 # def profile_page():
 def catalog_page():
     # return "Я главная каталога"
     return render_template('main.html')
 
 # Создаем вьюшку, используя в декораторе блюпринт вместо арр
-@catalog_blueprint.route('/catalog/<cat>/')
+@catalog_blueprint.route('/<cat>/')
 # def profilepage(cat):
 def category_page(cat):
     """
@@ -24,7 +27,7 @@ def category_page(cat):
     # return f'Я ст?раничка каталога категории {cat}'
     return render_template('category.html')
 
-@catalog_blueprint.route('/catalog/<cat>/<int:item>/')
+@catalog_blueprint.route('/<cat>/<int:item>/')
 def item_page(cat, item):
     """
     страничка товара
