df165981798d7f801f0febaa80f66e39b628a6a6
Axl-M <axlmalyasov@gmail.com>
2023-02-13 14:42:06 +1000

    search posts added

--- Lesson-12_homework/functions.py
+++ Lesson-12_homework/functions.py
@@ -1,1 +1,10 @@
 import json
+
+POST_PATH = "posts.json"
+
+def load_posts():
+    with open('posts.json', 'r', encoding='utf-8') as file:
+        posts = json.load(file)
+    return posts
+
+
--- Lesson-12_homework/main/main.py
+++ Lesson-12_homework/main/main.py
@@ -1,7 +1,18 @@
 from flask import Blueprint, request, render_template
+# запуск приложения будет из app (находится на одном уровне с function)
+# все будет ок, несмотря на то что здесь function не видит.
+from functions import load_posts
+
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
 
 @main_blueprint.route('/')
 def main():
     return render_template('index.html')
+
+
+@main_blueprint.route('/search/')
+def search():
+    search_by = request.args['s']
+    posts = [x for x in load_posts() if search_by.lower() in x['content'].lower()]
+    return render_template('post_list.html', search_by=search_by, posts=posts)
